79fdf748964e1a0cb2481eb8f54be91b708365b2
Arthur <aandre@netexpo.fr>
2020-12-16 16:13:58 +0100

    blue white red

--- netexpo-formation-ionic/src/app/menu/menu.component.ts
+++ netexpo-formation-ionic/src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -8,7 +8,31 @@ import { MenuController } from '@ionic/angular';
 })
 export class MenuComponent {
 
-  constructor(private menu: MenuController) { }
+  @Input() key1: string;
+  @Input() key2: string;
+  @Input() key3: string;
+
+  constructor(private menu: MenuController) {
+
+    setInterval(() => {
+      this.key1 = this.makeid(15);
+      this.key2 = this.makeid(15);
+      this.key3 = this.makeid(15);
+    }, 50);
+
+   
+
+   }
+
+   makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 
   ngOnInit() {}
 
