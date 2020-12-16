a1d942c528428f58bd1cc0d06c514fbec23712d2
gfoulon <gfoulon@netexpo.fr>
2020-12-16 16:02:15 +0100

    Ajout input sur Body

--- netexpo-formation-ionic/src/app/home/body/body.component.ts
+++ netexpo-formation-ionic/src/app/home/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit {
+
+  @Input() message: string;
 
   constructor() { }
 
