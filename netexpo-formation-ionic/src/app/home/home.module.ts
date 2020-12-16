2e9cc8862b9c28ff5dd62bb08b2bc24f51300096
Arthur <aandre@netexpo.fr>
2020-12-16 14:46:28 +0100

    push

--- netexpo-formation-ionic/src/app/home/home.module.ts
+++ netexpo-formation-ionic/src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HelpComponent } from './help/help.component';
+import { MenuComponent } from '../menu/menu.component';
 
 
 @NgModule({
@@ -14,6 +16,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, MenuComponent]
 })
 export class HomePageModule {}
